108be26e0e6da5be50ef5a49680cb804cb8d8501
syborg2290 <kasun.thaksala@gmail.com>
2023-04-21 19:56:31 +0530

    register user

--- Blockchain/node-api/api/index.js
+++ Blockchain/node-api/api/index.js
@@ -1,31 +1,32 @@
-const express = require('express')
+const express = require("express");
 
-const patient = require('./routes/patient')
-const hospital = require('./routes/hospital')
-const doctor = require('./routes/doctor')
-const pathlab = require('./routes/pathlab')
-const pharmacies = require('./routes/pharmacies')
-const general = require('./routes/general')
+const patient = require("./routes/patient");
+const hospital = require("./routes/hospital");
+const doctor = require("./routes/doctor");
+const pathlab = require("./routes/pathlab");
+const pharmacies = require("./routes/pharmacies");
+const general = require("./routes/general");
+const user = require("./routes/user");
 
-PORT = process.env.PORT || 3000
+PORT = process.env.PORT || 3000;
 
-
-const app = express()
-const logger = (req,res,next)=>{
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
-    next()
-}
+const app = express();
+const logger = (req, res, next) => {
+  console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
+  next();
+};
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(logger)
+app.use(logger);
 
-app.use('/patient',patient)
-app.use('/hospital',hospital)
-app.use('/doctor',doctor)
-app.use('/pathlab',pathlab)
-app.use('/pharmacies',pharmacies)
-app.use('/',general)
+app.use("/patient", patient);
+app.use("/hospital", hospital);
+app.use("/doctor", doctor);
+app.use("/pathlab", pathlab);
+app.use("/pharmacies", pharmacies);
+app.use("/user", user);
+app.use("/", general);
 
-app.listen(PORT,()=>{
-    console.log(`listening on port: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`);
+});
